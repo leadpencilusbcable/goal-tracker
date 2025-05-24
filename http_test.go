d8b746cb9b9fd0ae2bfc8f8cfd62a78676b62421
leadpencilusbcable <alexdel240@gmail.com>
2025-05-24 10:45:43 +1000

    Fix date test

--- http_test.go
+++ http_test.go
@@ -9,13 +9,16 @@ func TestGoalStatus(t *testing.T) {
 	now := time.Now()
 	now_date_str := now.Format(time.DateOnly)
 
+	//set now to start of day so it is not ahead of end_date when parsed
+	now, _ = time.Parse(time.DateOnly, now_date_str)
+
 	goal := Goal{ end_date: now_date_str }
 	status, err := getGoalStatus(goal, &now)
 
 	if err != nil {
 		t.Errorf("error determining goal status. %s", err.Error())
 	} else if status != "In progress" {
-		t.Errorf("expected status In progress. Got:  %s", status)
+		t.Errorf("expected status In progress. Got: %s", status)
 	}
 
 	yesterday := now.Add(-time.Hour * 24)
@@ -25,14 +28,14 @@ func TestGoalStatus(t *testing.T) {
 	if err != nil {
 		t.Errorf("error determining goal status. %s", err.Error())
 	} else if status != "Failed" {
-		t.Errorf("expected status Failed. Got:  %s", status)
+		t.Errorf("expected status Failed. Got: %s", status)
 	}
 
 	goal.completed_datetime = &now
 	status, err = getGoalStatus(goal, &yesterday)
 
 	if status != "Complete" {
-		t.Errorf("expected status Complete. Got:  %s", status)
+		t.Errorf("expected status Complete. Got: %s", status)
 	}
 }
 
